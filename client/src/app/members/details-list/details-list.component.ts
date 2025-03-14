0c991d88c522ce67dcb044f9fabe04627d9c8e6d
Ashish Satpute <ashishsatpute1808@gmail.com>
2025-03-15 00:37:25 +0530

    End of section 6

---
+++ client/src/app/members/details-list/details-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-list.component.html',
+  styleUrl: './details-list.component.css'
+})
+export class DetailsListComponent {
+
+}
